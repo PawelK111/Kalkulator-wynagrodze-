1776664b0efc05798f866af20c1004102f13511b
Paweł K <pekulewicz@gmail.com>
2019-02-21 22:41:12 +0100

    First commit

---
+++ Wynagrodzenia/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wynagrodzenia
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        decimal podstawowa;
+        decimal dodatkii;
+        decimal brutto;
+        decimal spoleczne;
+        decimal podatek;
+        public MainWindow()
+        {
+            InitializeComponent();   
+        }
+
+        private void Oblicz_button_Click(object sender, RoutedEventArgs e)
+        {
+            
+                if (placapodstText.Text == "")
+                {
+                    MessageBox.Show("Wprowadź płacę zasadniczą!", "Błąd wyliczenia", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+                else
+                {
+                    try
+                    {
+                        Wynagrodzenie addwynagrodzenie = new Wynagrodzenie();
+                        podstawowa = decimal.Parse(placapodstText.Text);
+                        if (dodatkiText.Text == "")
+                        {
+                            dodatkii = 0;
+                            dodatkiText.Text = "0";
+                        }
+                        else
+                        {
+                            dodatkii = decimal.Parse(dodatkiText.Text);
+                        }
+                        if (nieskładkoweText.Text == "")
+                        {
+                            nieskładkoweText.Text = "0";
+                        }
+
+                        brutto = podstawowa + dodatkii;
+                        spoleczne = (brutto - (brutto * 0.0976M) - (brutto * 0.015M) - (brutto * 0.0245M));
+                        podatek = (((spoleczne - 111.25M) * 0.18M) - 46.33M) - ((spoleczne) * 0.0775M);
+
+                        addwynagrodzenie.placa_podstawowa = podstawowa.ToString("c");
+                        addwynagrodzenie.dodatki = dodatkii.ToString("c");
+                        addwynagrodzenie.wynagr_brutto = brutto.ToString("c");
+                        addwynagrodzenie.ubezp_emerytalne = (brutto * 0.0976M).ToString("c");
+                        addwynagrodzenie.ubezp_rentowe = (brutto * 0.015M).ToString("c");
+                        addwynagrodzenie.ubezp_chorobowe = (brutto * 0.0245M).ToString("c");
+                        addwynagrodzenie.ubezp_zdrowotne = (spoleczne * 0.09M).ToString("c");
+                        addwynagrodzenie.podatekUS = Math.Round(podatek, 0).ToString("c");
+
+
+                        addwynagrodzenie.wynagr_netto = ((brutto - (brutto * 0.0976M) - (brutto * 0.015M) - (brutto * 0.0245M) - (spoleczne * 0.09M) - Math.Round(podatek, 0)) + decimal.Parse(nieskładkoweText.Text)).ToString("c");
+
+
+                        Lista_plac.Items.Add(addwynagrodzenie);
+                        excelButton.IsEnabled = true;
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Wprowadzono błędne dane!", "Błąd wyliczenia", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+        }
+    }  
+}
---
+++ Wynagrodzenia/Wynagrodzenie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wynagrodzenia
+{
+    public class Wynagrodzenie
+    {
+        public string placa_podstawowa { get; set; }
+        public string dodatki { get; set; }
+        public string wynagr_brutto { get; set; }
+        public string ubezp_emerytalne { get; set; }
+
+        public string ubezp_rentowe { get; set; }
+        public string ubezp_chorobowe { get; set; }
+        public string ubezp_zdrowotne { get; set; }
+        public string podatekUS { get; set; }
+        public string wynagr_netto { get; set; }
+
+    }
+}
